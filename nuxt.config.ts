dd0b46834b5f4f0ed87014950ac03cf2c070fdcf
Enrique Aparicio Castellanos <enrique@thesustainables.nl>
2023-09-08 17:50:16 +0200

    content and tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/content'],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
---
+++ pages/prueba.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1 class="text-3xl font-bold text-red-400">
+      Hello world!
+    </h1>
+  </template>
