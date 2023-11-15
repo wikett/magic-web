51a626a7c86936029bca4f86522286128ec8b3ff
Quique <enrique.ac9@gmail.com>
2023-11-15 15:41:52 +0100

    update format

--- pages/index.vue
+++ pages/index.vue
@@ -43,11 +43,11 @@
           <h2 class="mx-auto max-w-2xl text-3xl font-bold tracking-tight text-white sm:text-4xl">Marcas que recomendamos</h2>
           <p class="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">Después de mucho limpiar y probar muchos productos, podemos recomendar algunas marcas que realmente nos han funcionado</p>
           <div class="mx-auto mt-20 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-12 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 sm:gap-y-14 lg:max-w-4xl lg:grid-cols-5">
-            <a href="https://amzn.to/40Ggm4S"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/amazon-basics.svg" alt="Amazon Basics" width="158" height="48" /></a>
-            <a href="https://amzn.to/3MI7uWC"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/vanish.svg" alt="Vanish" width="158" height="48" /></a>
-            <a href="https://amzn.to/3syLiHw"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/fairy.svg" alt="Fairy" width="158" height="48" /></a>
-            <a href="https://amzn.to/47f4oS3"><img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="/img/frosch.svg" alt="Frosch" width="158" height="48" /></a>
-            <a href="https://amzn.to/3SHw79z"><img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="/img/sanytol.svg" alt="Sanytol" width="158" height="48" /></a>
+            <a href="https://amzn.to/40Ggm4S"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/amazon-basics.webp" alt="Amazon Basics" width="158" height="48" /></a>
+            <a href="https://amzn.to/3MI7uWC"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/vanish.webp" alt="Vanish" width="158" height="48" /></a>
+            <a href="https://amzn.to/3syLiHw"><img class="col-span-2 max-h-12 w-full object-contain lg:col-span-1" src="/img/fairy.webp" alt="Fairy" width="158" height="48" /></a>
+            <a href="https://amzn.to/47f4oS3"><img class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1" src="/img/frosch.webp" alt="Frosch" width="158" height="48" /></a>
+            <a href="https://amzn.to/3SHw79z"><img class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1" src="/img/sanytol.webp" alt="Sanytol" width="158" height="48" /></a>
           </div>
           <div class="absolute -top-24 right-0 -z-10 transform-gpu blur-3xl" aria-hidden="true">
             <div class="aspect-[1404/767] w-[87.75rem] bg-gradient-to-r from-[#80caff] to-[#4f46e5] opacity-25" style="clip-path: polygon(73.6% 51.7%, 91.7% 11.8%, 100% 46.4%, 97.4% 82.2%, 92.5% 84.9%, 75.7% 64%, 55.3% 47.5%, 46.5% 49.4%, 45% 62.9%, 50.3% 87.2%, 21.3% 64.1%, 0.1% 100%, 5.4% 51.1%, 21.4% 63.9%, 58.9% 0.2%, 73.6% 51.7%)" />
